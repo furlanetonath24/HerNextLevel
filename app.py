288567a46dad06869555fddcbd45412a57f5e6ee
Prisha Hemant Khanna <Prisha-Khanna@users.noreply.huggingface.co>
2026-08-13 19:03:14 +0000

    Fixed syntax for examples list since it was a nested list!

--- app.py
+++ app.py
@@ -141,13 +141,13 @@ with gr.Blocks(theme=gr.Theme.from_hub("Ayaku/Aa")) as demo:
     )
     
     # CHANGE 4: PLACED THE CHAT INTERFACE INSIDE THE BLOCKS WRAPPER
-    # Changed history format tracking to 'type="messages"' to keep your RAG history perfectly synced.
     gr.ChatInterface(
         respond, 
-        examples = ["What tech opportunities can I apply for?",
-                    "What programs are available for girls interested in technology?", 
-                    "Give me ideas for a passion project in tech", 
-                    "How can I start learning programming?"
+        examples = [
+                   ["What tech opportunities can I apply for?"],
+                   ["What programs are available for girls interested in technology?"], 
+                   ["Give me ideas for a passion project in tech"], 
+                   ["How can I start learning programming?"]
                    ],
         description="This Chatbot is for girls interested in tech who want more guidance on opportunities, programs, and passion project ideas. It provides you with resources that you can choose from.",
     )
