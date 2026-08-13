5a455f287c37a4be2eb2f6a0899f7b9094e2a1b9
Prisha Hemant Khanna <Prisha-Khanna@users.noreply.huggingface.co>
2026-08-13 18:39:10 +0000

    Deleted the type = message parameter.

--- app.py
+++ app.py
@@ -144,7 +144,6 @@ with gr.Blocks(theme=gr.Theme.from_hub("Ayaku/Aa")) as demo:
     # Changed history format tracking to 'type="messages"' to keep your RAG history perfectly synced.
     gr.ChatInterface(
         respond, 
-        type="messages",
         description="This Chatbot is for girls interested in tech who want more guidance on opportunities, programs, and passion project ideas. It provides you with resources that you can choose from.",
     )
 # CHANGE 5: LAUNCH THE NEW 'demo' BLOCK INSTEAD OF THE OLD 'chatbot' WRAPPER
